838900616e8c659a3abdf85c6c3a697c55b3ce8a
Airaken <lucho92p@hotmail.com>
2018-02-26 02:18:33 -0500

    kl
    kl

--- WebContent/js/chart-data.js
+++ WebContent/js/chart-data.js
@@ -1,20 +1,10 @@
 var randomScalingFactor = function(){ return Math.round(Math.random()*1000 + 3)};
 		
 	var lineChartData = {
-			labels : ["January","February","March","April","May","June","July"],
+			labels : ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio"],
 			datasets : [
 				{
 					label: "My First dataset",
-					fillColor : "rgba(220,220,220,0.2)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(220,220,220,1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-				},
-				{
-					label: "My Second dataset",
 					fillColor : "rgba(128,130,228,0.6)",
 					strokeColor : "rgba(128,130,228,1)",
 					pointColor : "rgba(128,130,228,1)",
@@ -26,17 +16,11 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000 + 3)}
 			]
 
 		}
-		
+	
+
 	var barChartData = {
-			labels : ["January","February","March","April","May","June","July"],
+			labels : ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio"],
 			datasets : [
-				{
-					fillColor : "rgba(220,220,220,0.5)",
-					strokeColor : "rgba(220,220,220,0.8)",
-					highlightFill: "rgba(220,220,220,0.75)",
-					highlightStroke: "rgba(220,220,220,1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-				},
 				{
 					fillColor : "rgba(128,130,228, 0.6)",
 					strokeColor : "rgba(128,130,228, 1)",
