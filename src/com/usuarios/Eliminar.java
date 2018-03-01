66bbce490350a8b46095f8e3d4f3ea0aea948e4a
Carlos E. Perez <carloseduardoperezmeza@outlook.com>
2018-02-28 23:49:08 -0500

    Eliminar usuario servlet minimizado

--- src/com/usuarios/Eliminar.java
+++ src/com/usuarios/Eliminar.java
@@ -1,43 +1,28 @@
 package com.usuarios;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.logica.ControladorBD;
 
-/**
- * Servlet implementation class eliminarUsuario
- */
 @WebServlet("/usuarios/eliminar")
 public class Eliminar extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	clases.usuario usuario = new clases.usuario();
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public Eliminar() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("/error.jsp");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String usuario = request.getParameter("usuario").toString();
 		ControladorBD.borrarItem("usuarios", "usuario", usuario);
