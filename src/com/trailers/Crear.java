c7003f438dc182a6350ff6c049c1d1d6f495306f
Carlos E. Perez <carloseduardoperezmeza@outlook.com>
2018-06-11 16:46:48 -0500

    Maricadita

--- src/com/trailers/Crear.java
+++ src/com/trailers/Crear.java
@@ -22,7 +22,7 @@ public class Crear extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("/404.jsp");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
