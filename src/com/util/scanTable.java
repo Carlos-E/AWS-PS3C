82a5da3ee757609968959b8646f27b16e5f124df
Carlos E. Perez <carloseduardoperezmeza@outlook.com>
2018-06-11 23:19:37 -0500

    Nuevo scanear con DynamoDB Directo

--- src/com/util/scanTable.java
+++ src/com/util/scanTable.java
@@ -1,7 +1,9 @@
 package com.util;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -17,6 +19,11 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.logica.ControladorBD;
@@ -25,15 +32,17 @@ import com.logica.ControladorBD;
 public class scanTable extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	//CODIGO DE PRUEBA
+	// CODIGO DE PRUEBA
 	BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAJSINT4F7K5BSGDRA",
 			"512NOFNfUl4hAZMyFEHpt7ygdmksBVzmfXr6xLsR");
 
 	AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
 			.withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
-	//CODIGO DE PRUEBA
 
-	
+	DynamoDB dynamoDB = new DynamoDB(client);
+
+	// CODIGO DE PRUEBA
+
 	public scanTable() {
 		super();
 	}
@@ -46,26 +55,26 @@ public class scanTable extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		// ObjectWriter ow = new
+		// ObjectMapper().writer().withDefaultPrettyPrinter();
+		// response.setContentType("application/json");
+		// response.getWriter().print(ow.writeValueAsString(ControladorBD.escanearTabla(request.getParameter("tabla"))));
+		// response.getWriter().close();
+
+		// CODIGO DE PRUEBA
+		Table table = dynamoDB.getTable(request.getParameter("tabla"));
+		ItemCollection<ScanOutcome> items = table.scan();
+
+		ArrayList<String> Items = new ArrayList<String>();
+		Iterator<Item> iterator = items.iterator();
+		while (iterator.hasNext()) {
+			Items.add(iterator.next().toJSON().toString());
+		}
 
 		response.setContentType("application/json");
-		//response.getWriter().print(ow.writeValueAsString(ControladorBD.escanearTabla(request.getParameter("tabla"))));
-		
-		//CODIGO DE PRUEBA
-		
-		Map<String, AttributeValue> expressionAttributeValues = new HashMap<String, AttributeValue>();
-
-		expressionAttributeValues.put(":val", new AttributeValue().withS("false"));
-
-		ScanRequest scanRequest = new ScanRequest().withTableName("reportes").withFilterExpression("visto = :val")
-				.withExpressionAttributeValues(expressionAttributeValues);
-
-		ScanResult result = client.scan(scanRequest);
-		
-		//CODIGO DE PRUEBA
-
-		response.getWriter().print(result.getItems());
+		response.getWriter().print(Items);
 		response.getWriter().close();
+		// CODIGO DE PRUEBA
 
 	}
 
