3dcd872009348194be0559070bccd5d7b858ca9e
Carlos E. Perez <carloseduardoperezmeza@outlook.com>
2018-08-12 15:26:41 -0500

    C

--- src/com/movil/Recogida.java
+++ src/com/movil/Recogida.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import clases.*;
 
 
-@WebServlet("/updateShipment")
+@WebServlet("/chequeo/recogida")
 public class Recogida extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	clases.Usuario usuario = new clases.Usuario();
