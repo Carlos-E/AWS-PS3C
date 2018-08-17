e2901caffb5d9366dff4815143177afa4cbfe6eb
Carlos E. Perez <carloseduardoperezmeza@outlook.com>
2018-08-17 00:30:52 -0500

    c

--- src/clases/DB.java
+++ src/clases/DB.java
@@ -17,7 +17,7 @@ public class DB extends DynamoDBMapper {
 
 	private final static String AccessKeyID = "AKIAJSINT4F7K5BSGDRA";
 	private final static String SecretKey = "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";
-	
+
 	private static Regions region = Regions.US_EAST_1;
 
 	private static BasicAWSCredentials basicCreds = new BasicAWSCredentials(AccessKeyID, SecretKey);
@@ -26,43 +26,79 @@ public class DB extends DynamoDBMapper {
 	public DB() {
 		super(AmazonDynamoDBClientBuilder.standard().withRegion(region).withCredentials(staticCreds).build());
 	}
-	
-	public double getTrailerSpace(String patente){
-		
-		double space=0;
+
+	public double getEspacioTrailer(String patente) {
+
+		double space = 0;
 
 		Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
 		eav.put(":v1", new AttributeValue().withS(patente));
 
 		List<Envio> EnviosEnTrailer = new DB().query(Envio.class,
 				new DynamoDBQueryExpression<Envio>().withIndexName("trailer").withConsistentRead(false)
-						.withKeyConditionExpression("trailer = :v1").withExpressionAttributeValues(eav));		
-		
-		for(int i=0;i<EnviosEnTrailer.size();i++){
+						.withKeyConditionExpression("trailer = :v1").withExpressionAttributeValues(eav));
+
+		for (int i = 0; i < EnviosEnTrailer.size(); i++) {
 			space = space + EnviosEnTrailer.get(i).getEspacio();
 		}
-		
+
 		return space;
 	}
-	
-	public double getVehicleSpace(String placa){
-		
-		double space=0;
+
+	public double getEspacioVehiculo(String placa) {
+
+		double space = 0;
 
 		Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
 		eav.put(":v1", new AttributeValue().withS(placa));
 
 		List<Envio> EnviosEnVehiculo = new DB().query(Envio.class,
 				new DynamoDBQueryExpression<Envio>().withIndexName("camion").withConsistentRead(false)
-						.withKeyConditionExpression("camion = :v1").withExpressionAttributeValues(eav));		
-		
-		for(int i=0;i<EnviosEnVehiculo.size();i++){
+						.withKeyConditionExpression("camion = :v1").withExpressionAttributeValues(eav));
+
+		for (int i = 0; i < EnviosEnVehiculo.size(); i++) {
 			space = space + EnviosEnVehiculo.get(i).getEspacio();
 		}
-		
+
 		return space;
 	}
-	
+
+	public double getPesoTrailer(String patente) {
+
+		double peso = 0;
+
+		Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+		eav.put(":v1", new AttributeValue().withS(patente));
+
+		List<Envio> EnviosEnTrailer = new DB().query(Envio.class,
+				new DynamoDBQueryExpression<Envio>().withIndexName("trailer").withConsistentRead(false)
+						.withKeyConditionExpression("trailer = :v1").withExpressionAttributeValues(eav));
+
+		for (int i = 0; i < EnviosEnTrailer.size(); i++) {
+			peso = peso + EnviosEnTrailer.get(i).getPeso();
+		}
+
+		return peso;
+	}
+
+	public double getPesoVehiculo(String placa) {
+
+		double peso = 0;
+
+		Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+		eav.put(":v1", new AttributeValue().withS(placa));
+
+		List<Envio> EnviosEnVehiculo = new DB().query(Envio.class,
+				new DynamoDBQueryExpression<Envio>().withIndexName("camion").withConsistentRead(false)
+						.withKeyConditionExpression("camion = :v1").withExpressionAttributeValues(eav));
+
+		for (int i = 0; i < EnviosEnVehiculo.size(); i++) {
+			peso = peso + EnviosEnVehiculo.get(i).getPeso();
+		}
+
+		return peso;
+	}
+
 	public List<Envio> getEnviosTrailer(String patente) {
 		Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
 		eav.put(":v1", new AttributeValue().withS(patente));
@@ -82,29 +118,28 @@ public class DB extends DynamoDBMapper {
 
 	}
 
-	
-	public boolean estaOcupado(String nombre, String vehiculo){
-		
+	public boolean estaOcupado(String nombre, String vehiculo) {
+
 		boolean resultado = false;
-		
+
 		List<Vehiculo> vehiculos = new DB().scan(Vehiculo.class, new DynamoDBScanExpression());
 		List<Trailer> trailers = new DB().scan(Trailer.class, new DynamoDBScanExpression());
-		
-		if(vehiculo.equals("null")){
-			for (int i = 0; i < vehiculos.size() ; i++){
-				if(nombre.equals(vehiculos.get(i).getUsuario())){
+
+		if (vehiculo.equals("null")) {
+			for (int i = 0; i < vehiculos.size(); i++) {
+				if (nombre.equals(vehiculos.get(i).getUsuario())) {
 					System.out.println("sale");
 					return true;
 				}
 			}
-		}else if (nombre.equals("null")){
-			for (int i = 0; i < trailers.size() ; i++){
-				if(vehiculo.equals(trailers.get(i).getCamion())){
+		} else if (nombre.equals("null")) {
+			for (int i = 0; i < trailers.size(); i++) {
+				if (vehiculo.equals(trailers.get(i).getCamion())) {
 					return true;
 				}
 			}
 		}
 		return resultado;
 	}
-	
+
 }
--- src/com/trailers/Modificar.java
+++ src/com/trailers/Modificar.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.logica.ControladorBD;
-
 import clases.DB;
 import clases.Trailer;
 
