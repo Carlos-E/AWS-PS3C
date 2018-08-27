f722b451fd3b5a18bf1a0f909aa8dbdaaec4dafc
Carlos E. Perez <carloseduardoperezmeza@outlook.com>
2018-08-27 18:28:58 -0500

    c

---
+++ src/clases/Email.java
@@ -0,0 +1,72 @@
+package clases;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Email {
+
+	static final String FROM = "noresponder@ps3c.com";
+	static final String FROMNAME = "PS3C Mail";
+
+	static final String SMTP_USERNAME = "sistemas@nauticaintegral.com";
+	static final String SMTP_PASSWORD = "JGzRbG8zpAD5sGkk";
+
+	static final String CONFIGSET = "ConfigSet";
+
+	static final String HOST = "snap.wnkserver8.com";
+
+	static final int PORT = 587;
+
+	public Email(String TO, String SUBJECT, String BODY) throws Exception {
+
+		// Create a Properties object to contain connection configuration
+		// information.
+		Properties props = System.getProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.port", PORT);
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.auth", "true");
+
+		// Create a Session object to represent a mail session with the
+		// specified properties.
+		Session session = Session.getDefaultInstance(props);
+
+		// Create a message with the specified information.
+		MimeMessage msg = new MimeMessage(session);
+		msg.setFrom(new InternetAddress(FROM, FROMNAME));
+		msg.setRecipient(Message.RecipientType.TO, new InternetAddress(TO));
+		msg.setSubject(SUBJECT);
+		msg.setContent(BODY, "text/html; charset=UTF-8");
+
+		// Add a configuration set header. Comment or delete the
+		// next line if you are not using a configuration set
+		// msg.setHeader("X-SES-CONFIGURATION-SET", CONFIGSET);
+
+		// Create a transport.
+		Transport transport = session.getTransport();
+
+		// Send the message.
+		try {
+			System.out.println("Sending...");
+
+			// Connect to Amazon SES using the SMTP username and password you
+			// specified above.
+			transport.connect(HOST, SMTP_USERNAME, SMTP_PASSWORD);
+
+			// Send the email.
+			transport.sendMessage(msg, msg.getAllRecipients());
+			System.out.println("Email sent!");
+		} catch (Exception ex) {
+			System.out.println("The email was not sent.");
+			System.out.println("Error message: " + ex.getMessage());
+		} finally {
+			// Close and terminate the connection.
+			transport.close();
+		}
+	}
+}
--- src/com/envios/Crear.java
+++ src/com/envios/Crear.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.logica.Dibujar;
+
 import clases.*;
 
 @WebServlet("/envios/crear")
@@ -27,19 +29,19 @@ public class Crear extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
-				
+
 		// GENERAR ENVIO
 		Envio envio = new Envio();
-		
+
 		if (request.getSession().getAttribute("rol").equals("cliente")) {
 			envio.setUsuario(request.getSession().getAttribute("username").toString().toLowerCase());
 		} else {
 			envio.setUsuario(request.getParameter("cliente").toLowerCase());
 		}
-		
+
 		Calendar calendar = Calendar.getInstance();
 		DecimalFormat mFormat = new DecimalFormat("00");
 
@@ -48,12 +50,12 @@ public class Crear extends HttpServlet {
 				+ mFormat.format(Double.valueOf(calendar.get(Calendar.DAY_OF_MONTH))) + " "
 				+ mFormat.format(calendar.get(Calendar.HOUR_OF_DAY)) + ":"
 				+ mFormat.format(calendar.get(Calendar.MINUTE)) + ":" + mFormat.format(calendar.get(Calendar.SECOND));
-		
+
 		envio.setFecha(fecha);
 
 		envio.setPeso(Double.valueOf(request.getParameter("peso").toLowerCase()));
 		envio.setEspacio(Double.valueOf(request.getParameter("espacio").toLowerCase()));
-		
+
 		envio.setEmpresa(request.getParameter("empresa").toLowerCase());
 		envio.setDestino(request.getParameter("destino"));
 		envio.setOrigen(request.getParameter("origen"));
@@ -61,33 +63,50 @@ public class Crear extends HttpServlet {
 		envio.setOrigenLatLong(request.getParameter("origenLatLong").toLowerCase());
 		envio.setTipo(request.getParameter("tipo").toLowerCase());
 		envio.setDescripcion(request.getParameter("descripcion").toLowerCase());
-		
-		
+
 		envio.setChequeoCarga(false);
 		envio.setChequeoDescarga(false);
 		envio.setEstado("no asignado");
 		envio.setCamion("ninguno");
 		envio.setTrailer("ninguno");
-		
-		//Guardar Envio en base de datos
+
+		// Guardar Envio en base de datos
 		new DB().save(envio);
 		// GENERAR ENVIO
 
-		
 		// GENERAR REPORTE
 		Reporte reporte = new Reporte();
-		
+
 		reporte.setUsuario(envio.getUsuario());
 		reporte.setHora(fecha);
 		reporte.setNota("Hay un nuevo envio del cliente: " + envio.getUsuario() + " Con Fecha: " + fecha);
 		reporte.setVisto(false);
-		
-		//Guardar Reporte en base de datos
+
+		// Guardar Reporte en base de datos
 		new DB().save(reporte);
 		// GENERAR REPORTE
-		
-		com.logica.Dibujar.mensaje(response.getWriter(), "Operacion Exitosa, Reporte generado.",
-				request.getContextPath() + "/envios/crear.jsp");
+
+		// ENVIAR CORREO
+		try {
+
+			Usuario usuario = new DB().load(Usuario.class, envio.getUsuario());
+			new Email(usuario.getCorreo(), "Notificación de envio PS3C",
+					String.join(System.getProperty("line.separator"), "<h2>Envio creado</h2>", 
+							"<p>Origen: ",envio.getOrigen(), 
+							"<br>Destino:", envio.getDestino(), 
+							"<br>Espacio: ",String.valueOf(envio.getEspacio()), "metros cubicos <br>Peso: ",
+							String.valueOf(envio.getPeso()), "Kg<br>Tipo:", envio.getTipo(), "<br>Descripci&oacute;n: ",
+							envio.getDescripcion(), "<br>Estado del env&iacute;o: ",envio.getEstado()," </p>"));
+
+		} catch (Exception e) {
+
+			Dibujar.mensaje(response.getWriter(), "Operacion Exitosa, Reporte generado, el correo no se envio", "/envios/crear.jsp");
+			e.printStackTrace();
+
+		}
+		// ENVIAR CORREO
+
+		Dibujar.mensaje(response.getWriter(), "Operacion Exitosa, Reporte generado. Correo enviado", "/envios/crear.jsp");
 
 	}
 
