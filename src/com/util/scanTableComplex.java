5e7217f3bbf98a0f6b2f69c38806e40e5beddb0f
Carlos E. Perez <carloseduardoperezmeza@outlook.com>
2018-09-12 20:23:36 -0500

    c

---
+++ src/com/util/scanTableComplex.java
@@ -0,0 +1,89 @@
+package com.util;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
+
+@WebServlet("/scanTable")
+public class scanTableComplex extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	// CODIGO DE PRUEBA
+	BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAJSINT4F7K5BSGDRA",
+			"512NOFNfUl4hAZMyFEHpt7ygdmksBVzmfXr6xLsR");
+
+	AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1)
+			.withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
+
+	DynamoDB dynamoDB = new DynamoDB(client);
+	// CODIGO DE PRUEBA
+
+	public scanTableComplex() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("/404.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("application/json");
+		response.setCharacterEncoding("utf-8");
+
+		try {
+			
+			System.out.println("Scanning table: " + request.getParameter("tabla"));
+
+			Table table = dynamoDB.getTable(request.getParameter("tabla"));
+			ItemCollection<ScanOutcome> result = table.scan();
+			ArrayList<String> Items = new ArrayList<String>();
+			Iterator<Item> iterator = result.iterator();
+			while (iterator.hasNext()) {
+				Items.add(iterator.next().toJSON().toString());
+			}
+			response.setContentType("application/json");
+			response.getWriter().print(Items);
+			response.getWriter().close();
+
+		} catch (Exception e) {
+			// com.logica.Dibujar.mensaje(response.getWriter(),
+			// "Ocurrio un error al intentar escanear la tabla: " +
+			// request.getParameter("tabla"));
+
+			response.setStatus(500);
+			response.getWriter().write(new ObjectMapper().writeValueAsString(new HashMap<String, String>() {
+				private static final long serialVersionUID = 1L;
+				{
+					put("title", "Operaci&oacute;n fallida");
+					put("message", "Ocurrio un error al intentar escanear la tabla: " + request.getParameter("tabla"));
+				}
+			}));
+			return;
+		}
+
+	}
+
+}
